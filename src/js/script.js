76fbabdd2f9545394a1466d2b1c23de46a6fb45e
Alexander <stwork87@gmail.com>
2019-09-23 22:07:22 +0300

    added styles for about-us section

--- src/js/script.js
+++ src/js/script.js
@@ -26,3 +26,35 @@ svg4everybody();
     cross.style.display = 'none';
   }
 })();
+//banner slider
+(function(){
+  var index = 1;
+  showSlides(index);
+  function minusSlide(){
+    index -= 1;
+    showSlides(index);
+  };
+  function plusSlide(){
+    index +=1;
+    showSlides(index);
+  };
+  function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName('goods-slider__item');
+    var indikators= document.getElementsByClassName('goods-slider__indicators-item');
+    document.querySelector('.goods-slider__controls-prev').onclick = minusSlide;
+    document.querySelector('.goods-slider__controls-next').onclick = plusSlide;
+    if (n > slides.length){
+      index = 1;
+    }
+    if (n < 1){
+      index = slides.length;
+    }
+    for (i=0; i< slides.length; i++){
+      slides[i].style.display = 'none';
+      indikators[i].style.backgroundColor = 'inherit';
+    }
+    slides[index - 1].style.display = 'block';
+    indikators[index - 1].style.backgroundColor = '#ffffff';
+  };
+})();
